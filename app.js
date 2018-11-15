66e16dc41e86c2048a3e1c96c214bc693fc22c47
Yousuf Khan <m.youxuf@gmail.com>
2018-11-16 02:41:12 +0500

    edit view and edit button with routes

--- app.js
+++ app.js
@@ -50,7 +50,19 @@ app.get('/ideas', (req,res)=>{
 
 //Add Idea Form Route
 app.get('/ideas/add', (req, res) => {
-    res.render('ideas/add')
+    res.render('ideas/add');
+});
+
+//Edit Idea Form Route
+app.get('/ideas/edit/:id', (req, res) => {
+    Idea.findOne({
+        _id: req.params.id
+    })
+    .then(idea =>{
+        res.render('ideas/edit',{
+            idea:idea
+        });
+    })
 });
 
 //Process form
